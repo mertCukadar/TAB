d11424132547582dc4740acc279aa9a5f07ef00d
cukadarMert <cukadar.mertkaan@gmail.com>
2024-07-21 00:08:02 +0300

    api updates for local host

--- src/app/api/index.tsx
+++ src/app/api/index.tsx
@@ -1,12 +1,19 @@
-
 const API_URL = 'http://localhost:8000/api/';
-export function FetchBlogs() {
-    return fetch(`${API_URL}blog/`)
-        .then((res) => res.json())
-        .then((data) => data);
-    }
-export function FetchBlog(id: number) {
-    return fetch(`${API_URL}blog/${id}/`)
-        .then((res) => res.json())
-        .then((data) => data);
-    }
+
+// api.ts
+
+export const FetchBlogs = async (page: number = 1) => {
+    const response = await fetch(`${API_URL}blog?limit=5&offset=${(page - 1) * 5}`);
+    const data = await response.json();
+    return data;
+};
+
+
+export const FetchBlog = async (id: number) => {
+  const response = await fetch(`${API_URL}blog/${id}/`);
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  const data = await response.json();
+  return data;
+};
