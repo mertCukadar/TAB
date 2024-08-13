7e0c2587e5be79380828e1b8e20b1ef6381b8299
mertCukadar <cukadar.mertkaan@gmail.com>
2024-08-14 01:10:49 +0300

    analytic script added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,13 +3,12 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import "./globals.css";
 import { Roboto } from 'next/font/google'
+import Script from 'next/script';
 
 const inter = Roboto({
   weight: '400',
   subsets: ['latin'],
 })
-
-
 
 export default function RootLayout({
   children,
@@ -19,6 +18,20 @@ export default function RootLayout({
   return (
     <html lang="tr">
       <body className={inter.className}>
+        {/* Google Analytics Script */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-693BHYFH1L"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-693BHYFH1L');
+          `}
+        </Script>
+        
         <Header />
           {children}
         <Footer />
