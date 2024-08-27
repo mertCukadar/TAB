b178bad86d2336a7be1b632dd97efca44935c809
cukadarMert <cukadar.mertkaan@gmail.com>
2024-08-27 14:12:01 +0300

    meta data test

--- src/pages/blog/[postname].tsx
+++ src/pages/blog/[postname].tsx
@@ -3,6 +3,7 @@ import { GetStaticProps, GetStaticPaths } from 'next';
 import { getPostBySlug, getPostSlugs } from '../../lib/posts';
 import PageLayout from '../../components/PageLayout';
 import Script from 'next/script';
+import { Metadata, ResolvingMetadata } from 'next';
 
 const MarkdownPreview = dynamic(() => import('@uiw/react-markdown-preview'), { ssr: false });
 
@@ -10,15 +11,38 @@ interface BlogPostProps {
   title: string;
   date: string;
   content: string;
+  description?: string;
+}
+
+export async function generateMetadata({ title, description }: BlogPostProps, parent: ResolvingMetadata): Promise<Metadata> {
+  const defaultDescription = description || 'Tab Mert Blog';
+  
+  return {
+    title: title,
+    description: defaultDescription,
+    openGraph: {
+      type: 'website',
+      title: title,
+      description: defaultDescription,
+  
+    },
+    twitter: {
+      card: 'summary_large_image',
+      site: '@tabmert',
+      description: defaultDescription,
+      
+    },
+  };
 }
 
 export default function BlogPost({ title, date, content }: BlogPostProps) {
+  
   return (
     <PageLayout>
-         <Script
-                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2073799749748484"
-                strategy="afterInteractive"
-            />
+      <Script
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2073799749748484"
+        strategy="afterInteractive"
+      />
       <div className="min-h-screen flex flex-col justify-center items-center bg-backgroundDefault p-6">
         <article className="max-w-4xl w-full">
           <header className="mb-12 text-center">
@@ -65,6 +89,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
       title: post.data.title,
       date: post.data.date,
       content: post.content,
+      description: post.data.description ?? '', // Varsayılan bir açıklama verilebilir
     },
   };
 };
